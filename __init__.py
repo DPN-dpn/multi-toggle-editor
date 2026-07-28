f95d6535524fafce72aa3969c0348b3e16f3e78b
DPN-dpn <doopaang@gmail.com>
2026-07-28 09:12:57 +0900

    0.2.0 버전 표기

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "멀티 토글 매니저",
     "author": "DPN",
-    "version": (0, 1, 0),
+    "version": (0, 2, 0),
     "blender": (2, 80, 0),
     "location": "3D 뷰 > 우측 UI 패널 > 멀티 토글",
     "description": "XXMI 모드의 멀티 토글을 쉽게 관리합니다.",
